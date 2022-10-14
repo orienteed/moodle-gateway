e8a7532ffd1afe32704a8b78768f824dff7b8670
jalvarez00 <jalvarez@orienteed.com>
2022-10-14 14:32:33 +0200

    feat: add metada

--- src/metadata/tags_metadata.py
+++ src/metadata/tags_metadata.py
@@ -1,10 +1,49 @@
 tags_metadata = [
-    {"name": "Authentication", "description": "Manage authentication. Authentication with Bearer token."},
+    {
+        "name": "Authentication",
+        "description": "Manage authentication. Authentication with Bearer token.",
+        "externalDocs": {
+            "description": "Moodle authentication API",
+            "url": "https://demo-moodle.orienteed.com/",
+        },
+    },
     {
         "name": "Courses",
         "description": "Manage courses.",
         "externalDocs": {
             "description": "Moodle courses API",
+            "url": "https://demo-moodle.orienteed.com/",
+        },
+    },
+    {
+        "name": "Completion",
+        "description": "Manage completion.",
+        "externalDocs": {
+            "description": "Moodle completion API",
+            "url": "https://demo-moodle.orienteed.com/",
+        },
+    },
+    {
+        "name": "Enrollment",
+        "description": "Manage enrollments.",
+        "externalDocs": {
+            "description": "Moodle enrollment API",
+            "url": "https://demo-moodle.orienteed.com/",
+        },
+    },
+    {
+        "name": "Media",
+        "description": "Request media content.",
+        "externalDocs": {
+            "description": "Moodle media API",
+            "url": "https://demo-moodle.orienteed.com/",
+        },
+    },
+    {
+        "name": "Users",
+        "description": "Modify users.",
+        "externalDocs": {
+            "description": "Moodle users API",
             "url": "https://demo-moodle.orienteed.com/",
         },
     },
